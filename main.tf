98ec82dc2b295427f7b0b2109c4820caff3fabe3
nagendra <n91k16@gmail.com>
2025-09-25 17:12:10 +0530

    creating resoure group

--- main.tf
+++ main.tf
@@ -45,83 +45,83 @@ resource "azurerm_subnet" "db_subnet" {
   address_prefixes     = ["15.0.3.0/24"] # Correction yahan hai
 }
 
-# ---------- NSGs ----------
-resource "azurerm_network_security_group" "app_nsg" {
-  name                = "app-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-resource "azurerm_network_security_group" "db_nsg" {
-  name                = "db-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# # ---------- NSGs ----------
+# resource "azurerm_network_security_group" "app_nsg" {
+#   name                = "app-nsg"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+# resource "azurerm_network_security_group" "db_nsg" {
+#   name                = "db-nsg"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-# ---------- NSG Rules for App ----------
-resource "azurerm_network_security_rule" "app_http" {
-  name                        = "allow-http"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.app_nsg.name
-}
-resource "azurerm_network_security_rule" "app_https" {
-  name                        = "allow-https"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.app_nsg.name
-}
-
-# # ---------- NSG Rules for DB ----------
-# resource "azurerm_network_security_rule" "db_sql" {
-#   name                        = "allow-sql-from-app"
+# # ---------- NSG Rules for App ----------
+# resource "azurerm_network_security_rule" "app_http" {
+#   name                        = "allow-http"
 #   priority                    = 100
 #   direction                   = "Inbound"
 #   access                      = "Allow"
 #   protocol                    = "Tcp"
 #   source_port_range           = "*"
-#   destination_port_range      = "1433"
-#   source_address_prefix       = "15.0.1.0/24" # Yahan app subnet ka range aayega
+#   destination_port_range      = "80"
+#   source_address_prefix       = "*"
 #   destination_address_prefix  = "*"
 #   resource_group_name         = azurerm_resource_group.rg.name
-#   network_security_group_name = azurerm_network_security_group.db_nsg.name
+#   network_security_group_name = azurerm_network_security_group.app_nsg.name
 # }
-
-# # Optional: explicit deny rule for extra security (not strictly needed, since Azure default rules deny all)
-# resource "azurerm_network_security_rule" "db_deny_internet" {
-#   name                        = "deny-internet"
-#   priority                    = 200
+# resource "azurerm_network_security_rule" "app_https" {
+#   name                        = "allow-https"
+#   priority                    = 110
 #   direction                   = "Inbound"
-#   access                      = "Deny"
-#   protocol                    = "*"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
 #   source_port_range           = "*"
-#   destination_port_range      = "*"
-#   source_address_prefix       = "Internet"
+#   destination_port_range      = "443"
+#   source_address_prefix       = "*"
 #   destination_address_prefix  = "*"
 #   resource_group_name         = azurerm_resource_group.rg.name
-#   network_security_group_name = azurerm_network_security_group.db_nsg.name
+#   network_security_group_name = azurerm_network_security_group.app_nsg.name
 # }
 
-# # ---------- Associate NSGs with Subnets ----------
-# resource "azurerm_subnet_network_security_group_association" "app_nsg_assoc" {
-#   subnet_id                 = azurerm_subnet.app_subnet.id
-#   network_security_group_id = azurerm_network_security_group.app_nsg.id
-# }
-# resource "azurerm_subnet_network_security_group_association" "db_nsg_assoc" {
-#   subnet_id                 = azurerm_subnet.db_subnet.id
-#   network_security_group_id = azurerm_network_security_group.db_nsg.id
-# }
+# # # ---------- NSG Rules for DB ----------
+# # resource "azurerm_network_security_rule" "db_sql" {
+# #   name                        = "allow-sql-from-app"
+# #   priority                    = 100
+# #   direction                   = "Inbound"
+# #   access                      = "Allow"
+# #   protocol                    = "Tcp"
+# #   source_port_range           = "*"
+# #   destination_port_range      = "1433"
+# #   source_address_prefix       = "15.0.1.0/24" # Yahan app subnet ka range aayega
+# #   destination_address_prefix  = "*"
+# #   resource_group_name         = azurerm_resource_group.rg.name
+# #   network_security_group_name = azurerm_network_security_group.db_nsg.name
+# # }
+
+# # # Optional: explicit deny rule for extra security (not strictly needed, since Azure default rules deny all)
+# # resource "azurerm_network_security_rule" "db_deny_internet" {
+# #   name                        = "deny-internet"
+# #   priority                    = 200
+# #   direction                   = "Inbound"
+# #   access                      = "Deny"
+# #   protocol                    = "*"
+# #   source_port_range           = "*"
+# #   destination_port_range      = "*"
+# #   source_address_prefix       = "Internet"
+# #   destination_address_prefix  = "*"
+# #   resource_group_name         = azurerm_resource_group.rg.name
+# #   network_security_group_name = azurerm_network_security_group.db_nsg.name
+# # }
+
+# # # ---------- Associate NSGs with Subnets ----------
+# # resource "azurerm_subnet_network_security_group_association" "app_nsg_assoc" {
+# #   subnet_id                 = azurerm_subnet.app_subnet.id
+# #   network_security_group_id = azurerm_network_security_group.app_nsg.id
+# # }
+# # resource "azurerm_subnet_network_security_group_association" "db_nsg_assoc" {
+# #   subnet_id                 = azurerm_subnet.db_subnet.id
+# #   network_security_group_id = azurerm_network_security_group.db_nsg.id
+# # }
 
